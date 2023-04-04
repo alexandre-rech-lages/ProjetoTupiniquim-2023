2cb93a7bacbc9456d0771582b05fe43031a03a3d
Alexandre Rech <alexandre.rech.lages@gmail.com>
2023-04-04 16:39:38 -0300

    Implementado a refatoração utilizando conceitos de oritanção a objetos

--- ProjetoTupiniquim.ConsoleApp/Program.cs
+++ ProjetoTupiniquim.ConsoleApp/Program.cs
@@ -15,48 +15,45 @@
 
 namespace ProjetoTupiniquim.ConsoleApp
 {
-    internal class Program
+
+    class Program
     {
         //variável é global, pode ser acessada dentro de todas as funções da classe Program
 
-        static string[] comandos = new string[2];
-        static string[] localizacoes = new string[2];
-        static int posicaoRobo = 0; 
+        static string[] comandos = new string[2] { "EMEMEMEMM", "MMDMMDMDDM" };
+        static string[] localizacoes = new string[2] { "1 2 N", "3 3 L" };
+        static int posicaoRobo = 0;
 
         static void Main(string[] args)
-        {            
-            ConfigurarArea();
+        {
+            //ConfigurarArea();
 
-            ConfigurarLocalizacaoEhComando();
+            //ConfigurarLocalizacaoEhComando();
 
-            ConfigurarLocalizacaoEhComando();
+            //ConfigurarLocalizacaoEhComando();
+
+            Robo[] tropaRobos = new Robo[2];
+            int i = 0;
 
             for (int j = 0; j < comandos.Length; j++)
             {
-                string[] inputRobo = localizacoes[j].Split(" ");
 
-                int posicaoX = Convert.ToInt32(inputRobo[0]);
-                int posicaoY = Convert.ToInt32(inputRobo[1]);
-
-                char direcao = Convert.ToChar(inputRobo[2]);
-
+                Robo robocop = new Robo(); //instância
+                robocop.Localizacao = localizacoes[j]; //1 2 N                
+                                
                 string comando = comandos[j];
 
-                ExplorarArea(comando, ref posicaoX, ref posicaoY, ref direcao);
+                robocop.ExplorarArea(comando);
 
-                ApresentarLocalizacaoRobo(posicaoX, posicaoY, direcao);
+                tropaRobos[i++] = robocop;
+
+                string localizacaoTeste = robocop.Localizacao;
+
+                Console.WriteLine(robocop.Localizacao);
             }
 
             Console.ReadLine();
         }
-
-        /**
-        * 1) Precisa ser "static"        
-        * 2) Definir o tipo de retorno "void"
-        * 3) Definir um nome "MoverRobo"
-        * 4) Definir se terá ou não argumentos "(char direcao, int posicaoX, int posicaoY)"
-        * 5) Definir um escopo
-        */
 
         static void ConfigurarLocalizacaoEhComando()
         {
@@ -75,88 +72,10 @@ namespace ProjetoTupiniquim.ConsoleApp
             string area = Console.ReadLine();
         }
 
-        static void ExplorarArea(string comando, ref int posicaoX, ref int posicaoY, ref char direcao)
-        {
-            char[] instrucoes = comando.ToCharArray();
 
-            for (int i = 0; i < instrucoes.Length; i++)
-            {
-                if (instrucoes[i] == 'M') //mover robo
-                {
-                    //posicaoX e posicaoY será passagem de parâmetros "por referência"
-                    MoverRobo(direcao, ref posicaoX, ref posicaoY);
-                }
 
-                else if (instrucoes[i] == 'E')
-                {
-                    //direção será passagem de parâmetros "por valor"
-                    direcao = VirarEsquerda(direcao);
-                }
 
-                else if (instrucoes[i] == 'D')
-                {
-                    //direção será passagem de parâmetros "por valor"
-                    direcao = VirarDireita(direcao);
-                }
-            }
-        }
-
-        static void ApresentarLocalizacaoRobo(int posicaoX, int posicaoY, char direcao)
-        {
-            Console.WriteLine(posicaoX + " " + posicaoY + " " + direcao);
-        }
-       
-        static void MoverRobo(char d, ref int pX, ref int pY)
-        {
-            if (d == 'N')
-                pY = pY + 1; //incrementar o Y
-
-            else if (d == 'O')
-                pX = pX - 1; //decrementar o X
-
-            else if (d == 'S')
-                pY = pY - 1; //decrementar o Y 
-
-            else if (d == 'L')
-                pX = pX + 1; //incrementar o X
-        }
-
-        static char VirarEsquerda(char direcaoAtual)
-        {
-            char novaDirecao = direcaoAtual;
-
-            if (direcaoAtual == 'N')
-                novaDirecao = 'O';
-
-            else if (direcaoAtual == 'O')
-                novaDirecao = 'S';
-
-            else if (direcaoAtual == 'S')
-                novaDirecao = 'L';
-
-            else if (direcaoAtual == 'L')
-                novaDirecao = 'N';
-
-            return novaDirecao;
-        }
-
-        static char VirarDireita(char direcaoAtual)
-        {
-            char novaDirecao;
-
-            switch (direcaoAtual)
-            {
-                case 'N': novaDirecao = 'L'; break;
-                case 'O': novaDirecao = 'N'; break;
-                case 'S': novaDirecao = 'O'; break;
-                case 'L': novaDirecao = 'S'; break;
-
-                default:
-                    novaDirecao = direcaoAtual;
-                    break;
-            }
-
-            return novaDirecao;
-        }
     }
+
+
 }
---
+++ ProjetoTupiniquim.ConsoleApp/Robo.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTupiniquim.ConsoleApp
+{
+    public class Robo //projeto, receita, design, molde
+    {
+        //atributos
+        private int posicaoX;
+        private int posicaoY;
+        private char direcao;
+
+        //propriedades
+        public string Localizacao
+        {
+            set  // modificar o valor dos atributos
+            {
+                string[] inputRobo = value.Split(" ");
+
+                if (inputRobo.Length != 3)
+                    Console.WriteLine("Localização inválida");
+                else
+                {                    
+                    posicaoX = Convert.ToInt32(inputRobo[0]);
+                    posicaoY = Convert.ToInt32(inputRobo[1]);
+                    direcao = Convert.ToChar(inputRobo[2]);
+                }
+            }
+            get
+            {
+                return posicaoX + " " + posicaoY + " " + direcao;
+            }
+        }
+
+        //métodos públicos
+        public void ExplorarArea(string comando)
+        {            
+            char[] instrucoes = comando.ToCharArray();
+
+            for (int i = 0; i < instrucoes.Length; i++)
+            {
+                if (instrucoes[i] == 'M') //mover robo
+                {
+                    //posicaoX e posicaoY será passagem de parâmetros "por referência"
+                    MoverRobo();
+                }
+
+                else if (instrucoes[i] == 'E')
+                {
+                    //direção será passagem de parâmetros "por valor"
+                    VirarEsquerda();
+                }
+
+                else if (instrucoes[i] == 'D')
+                {
+                    //direção será passagem de parâmetros "por valor"
+                    VirarDireita();
+                }
+            }
+        }
+
+
+        //métodos privados
+        private void MoverRobo()
+        {
+            if (direcao == 'N')
+                posicaoY = posicaoY + 1; //incrementar o Y
+
+            else if (direcao == 'O')
+                posicaoX = posicaoX - 1; //decrementar o X
+
+            else if (direcao == 'S')
+                posicaoY = posicaoY - 1; //decrementar o Y 
+
+            else if (direcao == 'L')
+                posicaoX = posicaoX + 1; //incrementar o X
+        }
+
+        private void VirarEsquerda()
+        {
+            if (direcao == 'N')
+                direcao = 'O';
+
+            else if (direcao == 'O')
+                direcao = 'S';
+
+            else if (direcao == 'S')
+                direcao = 'L';
+
+            else if (direcao == 'L')
+                direcao = 'N';
+        }
+
+        private void VirarDireita()
+        {
+            switch (direcao)
+            {
+                case 'N': direcao = 'L'; break;
+                case 'O': direcao = 'N'; break;
+                case 'S': direcao = 'O'; break;
+                case 'L': direcao = 'S'; break;
+
+                default:
+                    break;
+            }
+        }
+
+        
+    }
+}
